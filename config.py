9a4df5b447cefbe5e6ae697f05d26f87417ce245
Ic34al <138458465+Ic34al@users.noreply.github.com>
2025-03-21 21:21:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ APP_ID = int(os.environ.get("APP_ID", ""))
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002170811388"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002027914496"))
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "sewxiy")
 #OWNER ID
